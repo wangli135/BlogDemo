8943d4adfa0cbc13ac001e8a1cef20174e096557
xingfeng <wangli921016@gmail.com>
2016-09-11 13:14:11 +0800

    删除闲着无聊写的测试

--- textspan/src/test/java/com/xks/textspan/ExampleUnitTest.java
+++ textspan/src/test/java/com/xks/textspan/ExampleUnitTest.java
@@ -2,8 +2,6 @@ package com.xks.textspan;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
@@ -11,7 +9,5 @@ public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() throws Exception {
-        Add add = new Add();
-        assertEquals(add.add(2,4), 6);
     }
 }
